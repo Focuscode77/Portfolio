ec838e8d80dc1eb140d6bc3357357af87134a314
Joshua Coleman <50680485+Focuscode77@users.noreply.github.com>
2023-08-29 17:29:47 -0400

    Update Mediation.Js
    This the functionality for the meditation game

---
+++ js/meditation.js
@@ -0,0 +1,91 @@
+//meditationStartBtn()
+//meditationStart()
+//meditationStart
+//breatheIn
+//breatheOut
+//"meditationDirections"
+const displayNone = "displayNone";
+
+
+const meditationStrings = {
+
+    meditationDirections: "meditationDirections",
+    meditationStart: "meditationStart",
+    breatheIn: "breatheIn",
+    breatheOut: "breatheOut"
+
+
+}
+
+const resetMeditation = () => {
+    getById(meditationStrings.breatheOut).classList.add(displayNone);
+    getById(meditationStrings.meditationStart).classList.remove(displayNone);
+
+}
+
+
+const showBreatheIn = () => {
+
+    getById(meditationStrings.breatheIn).classList.remove(displayNone);
+    getById(meditationStrings.breatheOut).classList.add(displayNone);
+
+    console.log("Breathe In");
+
+}
+
+
+const showBreatheOut = () => {
+    getById(meditationStrings.breatheIn).classList.add(displayNone);
+    getById(meditationStrings.breatheOut).classList.remove(displayNone);
+
+    console.log("Breathe Out");
+
+
+}
+
+
+const showArrows = () => {
+    showBreatheIn();
+    setTimeout(() => {
+        showBreatheOut();
+
+    }, 3000);
+
+}
+
+const startMeditation = () => {
+
+    let delay = 0;
+    for (let i = 0; i < 4; i++) {
+
+        setTimeout(() => {
+            showArrows();
+
+        }, delay);
+        delay += 6000;
+    }
+    setTimeout(() => {
+
+        resetMeditation();
+
+    }, delay);
+}
+
+
+
+const meditationStartBtn = () => {
+    getById(meditationStrings.meditationStart).classList.add(displayNone);
+    getById(meditationStrings.meditationDirections).classList.remove(displayNone);
+    console.log("Starting")
+}
+
+
+
+
+const meditationStart = () => {
+
+    getById(meditationStrings.meditationDirections).classList.add(displayNone);
+    startMeditation();
+
+}
+
