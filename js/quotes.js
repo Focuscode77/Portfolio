c3aeec0297d8cf89ab793654bc4dca4cd4c29808
Joshua Coleman <50680485+Focuscode77@users.noreply.github.com>
2023-10-01 15:10:23 -0400

    Update quotes.js

--- js/quotes.js
+++ js/quotes.js
@@ -6,32 +6,32 @@ const quotes = [
     },
 
     {
-        "quote": "I hear and I forget. I see and I remember. I do and I understand.",
-        "author": "Confucius"
+        "quote": "Be the change that you wish to see in the world.",
+        "author": "Mahatma Gandhi"
 
     },
 
     {
-        "quote": "Real knowledge is to know the extent of one's ignorance.",
-        "author": "Confucius"
+        "quote": "You only live once, but if you do it right, once is enough",
+        "author": "Mae West"
 
     },
 
     {
-        "quote": "Wisdom is the right use of knowledge. To know is not to be wise. Many men know a great deal, and are all the greater fools for it. There is no fool so great a fool as a knowing fool. But to know how to use knowledge is to have wisdom",
-        "author": "Charles Spurgeon"
+        "quote": "“Be yourself; everyone else is already taken.",
+        "author": "Oscar Wild"
+
+    },
+
+    {
+        "quote": "A room without books is like a body without a soul.",
+        "author": "Marcus Tullius Cicero"
 
     },
 
     {
         "quote": "A people without the knowledge of their past history, origin and culture is like a tree without roots.",
         "author": "Marcus Garvey"
-
-    },
-
-    {
-        "quote": "Wisdom is the right use of knowledge. To know is not to be wise. Many men know a great deal, and are all the greater fools for it. There is no fool so great a fool as a knowing fool. But to know how to use knowledge is to have wisdom.",
-        "author": "Charles Spurgeon"
 
     },
 
